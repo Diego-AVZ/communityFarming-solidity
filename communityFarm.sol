1b1a88d5c36d41675690995e5115f7580c82425c
Diego AVZ <124291993+Diego-AVZ@users.noreply.github.com>
2023-10-10 13:29:47 +0200

    communityFarm.sol

--- communityFarm.sol
+++ communityFarm.sol
@@ -17,6 +17,8 @@ contract factory{
     }
 }
 
+/*_____________________________*/
+
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
@@ -41,7 +43,7 @@ contract communityFarm {
     address public owner;
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "not Owner of this C0ntract");
+        require(msg.sender == owner, "not Owner of this Contract");
         _;
     }
 
@@ -69,6 +71,12 @@ contract communityFarm {
     bool hasDepoA;
     bool hasDepoB;
 
+	struct depositsPerUser {
+		address user;
+		uint amountAInDol;
+		uint amountBInDol; // Is Stablecoin
+	}
+
 	function depositTokens(uint amountA, uint amountB) public{
 		//Token A
 		require(tokenA.approve(address(this), amountA), "Approval failed");
@@ -90,81 +98,153 @@ contract communityFarm {
 		depositList.push(newDepositsStruct);
 	}
 
-    function withdrawAllDeposits() public { // Dependiendo de la opcion elegida, aqui habría que calcular el IL
-        require(hasDepoA || hasDepoB);
-        require(tokenA.transferFrom(address(this), msg.sender, depositsA[msg.sender]));
-        require(tokenB.transferFrom(address(this), msg.sender, depositsB[msg.sender]));
-        depositsA[msg.sender] = 0;
+    
+
+		// PARA RETIROS
+			// userWithdrawFromUni() -> El usuario interactua con UNI y puede retirar al contrato lo que le corresponde de liquidez
+			// userWithdrawFromContract() -> si hay posición abierta, llama a la primera función y después El usuario retira toda la liquidez que le corresponde de este contrato
+			// haz otra f() en la que El owner cierra la posición y los usuarios pueden retirar con userWithdrawFromContract()
+
+	function userWithdrawFromUni() public {
+		require(hasDepoA || hasDepoB);
+		require(positionOpened == true);
+		
+		// Se llama a la función de 'retirar liquidez' de UNISWAP con el % que ha aportado el user
+
+		position memory lastPosition = positions[positions.length -1];
+		uint percA = lastPosition.percentA; // % que se ha usado en la lastPosition, esto es = lo que se ha usado de total deositado por msg.sender
+		uint percB = lastPosition.percentB;
+
+		uint usedAmountA = depositsA[msg.sender] * percA; // cantidad usada en la posición
+		uint usedAmountB = depositsB[msg.sender] * percB;
+		uint unUsedAmountA = depositsA[msg.sender] - usedAmountA;
+		uint unUsedAmountB = depositsB[msg.sender] - usedAmountB;
+
+		//Lo que queda en uniswap TRAS el IL
+		uint actualUniLiqA; // No se como obtenerlo
+		uint actualUniLiqB;
+		// cantidad que tiene ahora el usuario:
+		uint userActualAmountA = actualUniLiqA * percA; // Lo que hay que devolver al usuario
+		uint userActualAmountB = actualUniLiqB * percB;
+
+		require(tokenA.transferFrom(address(uniswap), address(this), userActualAmountA)); // Algo así
+		require(tokenB.transferFrom(address(uniswap), address(this), userActualAmountB));
+
+		// Una vez trasferidos los tokens a este contrato, se actualiza la cantidad depositada
+		// es la cantidad no usada en la posición + lo que habia en uniswap y que correspondía al usuario
+		depositsA[msg.sender] = unUsedAmountA + userActualAmountA; 
+		depositsB[msg.sender] = unUsedAmountB + userActualAmountB;
+
+		// Las userActualAmount estarán en este contrato y el usuario puede retirar todo = userActualAmountA y unUsedAmount
+		
+	
+	}
+
+	function userWithdrawFromContract() public {
+		require(hasDepoA || hasDepoB);
+		if(positionOpened == true){
+			userWithdrawFromUni();
+		}
+
+		totalAmountA = totalAmountA - depositsA[msg.sender];
+        totalAmountB = totalAmountB - depositsB[msg.sender]; // Se resta lo que habían depositado
+		
+		require(tokenA.transferFrom(address(this), msg.sender, depositsA[msg.sender]));
+		require(tokenB.transferFrom(address(this), msg.sender, depositsB[msg.sender]));
+
+		depositsA[msg.sender] = 0;                           // Despues se iguala a 0
         depositsB[msg.sender] = 0;
-        totalAmountA = totalAmountA - depositsA[msg.sender];
-        totalAmountB = totalAmountB - depositsB[msg.sender];
-        hasDepoB = false; hasDepoA = false;
-    }
+		
+	}
 
 	struct position {
 		uint date;
 		int priceA;
 		uint percentA;
 		uint percentB;
+	
 	}
+
+	bool public positionOpened;
 
 	position[] positions;
 
 	function addLiquidityToUni(uint amountLiqA, uint amountLiqB) public onlyOwner{
+		require(positionOpened == false, "Close Position, then open a new Position");
 		/*
 		Lógica para enviar a la  pool de Uniswap
 		*/
-		//registrar: del 'totalAmountA' y 'totalAmountB' los % que se han usado
-		uint percA = amountLiqA*100 / totalAmountA; // Lo que se ha usado en la operación
-		uint percB = amountLiqB*100 / totalAmountB;
+		uint percA = amountLiqA * 100 / totalAmountA; // Lo que se ha usado en la operación
+		uint percB = amountLiqB * 100 / totalAmountB;
 
 		position memory newPos = position(block.timestamp, getTokenAPrice(), percA, percB);
 		positions.push(newPos);
 
-		//percA y percB son los % que se usan y que se aplicarán a lo usado de cada usuario
+		
+		positionOpened = true;
 
 	}
+
+	function withdrawUniLiquidity() public onlyOwner {
+		positionOpened = false;
+	}
+
+	address protocolAddress = 0x3fE959Dc78ed8948831df6a5453f32fD8AEDA8de4 ;
 
 	function claimFromUni() public onlyOwner {
 		/*
-			Lógica para claimear
+			Lógica UNI para claim
 		*/
-		//Registrar la cantidad total de recompensas. se alamacena la recompensa en el contrato
-		//se reparten las recompensas entre los usuarios proveedores
-		uint totalFeesFromUni;
-		//Hay que calcular el porcentaje de liquidez que ha depositado cada usuario respecto al total
-		uint totalLiq = calcTotalLiq();
 		
-		//Repartir por % a Owner, a MyWallet y a cada user
+		uint tokenAFeesFromUni;
+		uint tokenBFeesFromUni;
+		uint totalLiq = calcTotalLiqA() + calcTotalLiqB() ;
+		uint userFee = 1 / 2;       // 50%
+		uint ownerFee = 9 / 20 ;     // 45%
+		uint protocolFee = 1 / 20 ;   // 5%
+
 		for(uint i = depositList.length - 1; i > 0; i--) {
 			depositsPerUser memory depoPerUser2 = depositList[i];
-			uint userPerc = calcPercDepUsers(depoPerUser2.user);
-			tokenA.transferFrom(address(this), depoPerUser2.user, totalFeesFromUni*userPerc);
-			tokenB.transferFrom(address(this), depoPerUser2.user, totalFeesFromUni*userPerc);
+			uint userPercA = calcPercDepUsersA(depoPerUser2.user);
+			uint userPercB = calcPercDepUsersB(depoPerUser2.user);
+			tokenA.transferFrom(address(this), depoPerUser2.user, tokenAFeesFromUni * userPercA * userFee);
+			tokenB.transferFrom(address(this), depoPerUser2.user, tokenBFeesFromUni * userPercB * userFee);
 		}
 
-	}
+		tokenA.transferFrom(address(this), owner, tokenAFeesFromUni * ownerFee);
+		tokenB.transferFrom(address(this), owner, tokenBFeesFromUni * ownerFee);
 
-	struct depositsPerUser {
-		address user;
-		uint amountAInDol;
-		uint amountBInDol; // Is Stablecoin
+		tokenA.transferFrom(address(this), protocolAddress, tokenAFeesFromUni * protocolFee);
+		tokenB.transferFrom(address(this), protocolAddress, tokenBFeesFromUni * protocolFee);
+
 	}
 
 	depositsPerUser public depoPerUser;
 
 	depositsPerUser[] depositList;
 
-	function calcPercDepUsers(address user) public view returns(uint) {
+	function calcPercDepUsersA(address user) public view returns(uint) {
 		for (uint32 i = 0; i < depositList.length; i++) {
 			if (user == depoPerUser.user){
-				uint percent = (uint(depoPerUser.amountAInDol) + uint(depoPerUser.amountBInDol))*100 / calcTotalLiq();
+				return(uint(depoPerUser.amountAInDol)*100 / calcTotalLiqA());
 			}
 		}
 	}
 
-	function calcTotalLiq() public view returns(uint) {
-		return(totalAmountA * uint(getTokenAPrice()) + totalAmountB);
+	function calcPercDepUsersB(address user) public view returns(uint) {
+		for (uint32 i = 0; i < depositList.length; i++) {
+			if (user == depoPerUser.user){
+				uint percent = uint(depoPerUser.amountBInDol)*100 / calcTotalLiqB();
+			}
+		}
+	}
+
+	function calcTotalLiqA() public view returns(uint) {
+		return(totalAmountA * uint(getTokenAPrice())); // $ totales depositados de A
+	}
+
+	function calcTotalLiqB() public view returns(uint) {
+		return(totalAmountB); // $ totales depositados de B
 	}
 
 	AggregatorV3Interface internal dataFeed;
